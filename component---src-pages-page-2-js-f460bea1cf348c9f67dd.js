0a77a7151db47d391e2b5ced55fe65a822edb00f
Yenny <aldald73@naver.com>
2022-03-13 21:34:51 +0900

    Updates

--- component---src-pages-page-2-js-f460bea1cf348c9f67dd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(7076),u=a(262);t.default=function(){return l.createElement(r.Layout,null,l.createElement(u.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-f460bea1cf348c9f67dd.js.map
